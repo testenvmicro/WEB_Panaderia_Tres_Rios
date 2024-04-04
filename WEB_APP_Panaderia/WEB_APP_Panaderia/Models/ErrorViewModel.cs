b755fcffb316ecaeaaaaba2e999fe7b28407f991
testenvmicro <114837714+testenvmicro@users.noreply.github.com>
2024-04-03 21:40:30 -0600

    Creacion proyecto

---
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_APP_Panaderia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
