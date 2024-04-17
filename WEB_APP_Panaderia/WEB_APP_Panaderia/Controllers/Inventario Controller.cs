1efada9c0e204cf75aa02e5da14aae1be49f1db6
Josué Alvarado <josuealvarados@hotmail.com>
2024-04-17 09:35:02 -0600

    listo

---
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Controllers/Inventario Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_APP_Panaderia.Controllers
+{
+	public class Inventario : Controller
+	{
+		public IActionResult InventarioLista()
+		{
+			return View();
+		}
+		public IActionResult NuevoInsumo()
+		{
+			return View();
+		}
+	}
+}
