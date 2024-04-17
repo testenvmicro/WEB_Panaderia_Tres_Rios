386ae9fd3eee311510d0f89c38f19d8d4649af5c
testenvmicro <114837714+testenvmicro@users.noreply.github.com>
2024-04-17 17:19:03 -0600

    Cambios Generales

---
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Controllers/AccountingController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WEB_APP_Panaderia.Controllers
+{
+    public class AccountingController : Controller
+    {
+            public IActionResult CrearCuenta()
+            {
+                return View();
+            }
+
+            public IActionResult EditarCuenta()
+            {
+                return View();
+            }
+
+            public IActionResult EliminarCuenta()
+            {
+                return View();
+            }
+
+            public IActionResult ListarCuentas()
+            {
+                return View();
+            }
+        
+
+            public IActionResult CrearTransaccion()
+            {
+                return View();
+            }
+
+            public IActionResult EditarTransaccion()
+            {
+                return View();
+            }
+
+            public IActionResult EliminarTransaccion()
+            {
+                return View();
+            }
+
+            public IActionResult ListarTransacciones()
+            {
+                return View();
+            }
+        public IActionResult ListarEstadoFinanciero()
+        {
+            return View();
+        }
+    }
+    }
+
