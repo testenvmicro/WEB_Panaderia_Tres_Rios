e43befe1388af2045e35f1cb617f37265ea537ef
Jonathan Anchia <jonhanchia2@gmail.com>
2024-04-17 05:43:04 -0600

    Jonathan Anchia, Notificaciones y Contabilidad

--- WEB_APP_Panaderia/WEB_APP_Panaderia/Controllers/HomeController.cs
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Controllers/HomeController.cs
@@ -46,5 +46,21 @@ namespace WEB_APP_Panaderia.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult ListaNotificaciones()
+        {
+            return View();
+        }
+        public IActionResult CrearNotificaciones()
+        {
+            return View();
+        }
+        public IActionResult EditarNotificaciones()
+        {
+            return View();
+        }
+        public IActionResult VerNotificacion()
+        {
+            return View();
+        }
     }
 }
