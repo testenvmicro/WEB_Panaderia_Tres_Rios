d77b1560ed8ba9f1b89adfd673d5a6528c05c7bf
testenvmicro <114837714+testenvmicro@users.noreply.github.com>
2024-04-17 18:46:59 -0600

    Ultimos Arreglos

---
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Controllers/CotizacionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_APP_Panaderia.Controllers
+{
+	public class CotizacionController : Controller
+	{
+		public IActionResult CotizacionesLista()
+		{
+			return View();
+		}
+		public IActionResult EditarCotizacion()
+		{
+			return View();
+		}
+	}
+}
---
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Controllers/Inventario Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_APP_Panaderia.Controllers
+{
+	public class Inventario : Controller
+	{
+		public IActionResult InventarioLista()
+		{
+			return View();
+		}
+		public IActionResult NuevoInsumo()
+		{
+			return View();
+		}
+		public IActionResult EditarInsumo()
+		{
+			return View();
+		}
+	}
+}
