48d819995676debf7c936437f455a5129a999d9c
testenvmicro <114837714+testenvmicro@users.noreply.github.com>
2024-05-26 14:18:04 -0600

    App Settings
    - Cambios Generales en el app settings para que pueda ver el API

--- WEB_APP_Panaderia/WEB_APP_Panaderia/Entities/UsuariosEntities.cs
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Entities/UsuariosEntities.cs
@@ -2,10 +2,10 @@
 {
     public class UsuariosEntities
     {
-        public long id_usuario { get; set; }
+        public long idUsuario { get; set; }
         public string correo { get; set; } = string.Empty;
         public string contrasenna { get; set; } = string.Empty;
         public bool estado { get; set; } = false;
-        public int id_rol { get; set; } = 2;
+        public int idRol { get; set; } = 2;
     }
 }
