638b12d2d69078395d63258bdf32012180d96404
testenvmicro <114837714+testenvmicro@users.noreply.github.com>
2024-06-26 20:35:35 -0600

    Bitacora de errores
    - Se crea la bitacora de errores y se hacen unas mejores en general

---
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Entities/LogsEntities.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_APP_Panaderia.Entities
+{
+	public class LogsEntities
+	{
+		public string Detalle { get; set; } = string.Empty;
+		public string Origen { get; set; } = string.Empty;
+		public int consecutivoUsuario { get; set; }
+	}
+}
--- WEB_APP_Panaderia/WEB_APP_Panaderia/Entities/UsuariosEntities.cs
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Entities/UsuariosEntities.cs
@@ -6,7 +6,8 @@
         public string correo { get; set; } = string.Empty;
         public string contrasenna { get; set; } = string.Empty;
         public bool estado { get; set; } = false;
-        public int idRol { get; set; } = 2;
-        public string nombre { get; set; } = string.Empty;
+        public int idRol { get; set; } = 0;
+		public string descripcion { get; set; } = string.Empty;
+		public string nombre { get; set; } = string.Empty;
 	}
 }
