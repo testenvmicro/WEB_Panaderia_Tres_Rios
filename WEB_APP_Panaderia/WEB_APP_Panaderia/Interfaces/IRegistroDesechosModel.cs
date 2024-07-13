1443b1bf33d80d42bc6cbc47278b56bfcbd10a0c
testenvmicro <114837714+testenvmicro@users.noreply.github.com>
2024-07-12 23:04:45 -0600

    Update IRegistroDesechosModel.cs

--- WEB_APP_Panaderia/WEB_APP_Panaderia/Interfaces/IRegistroDesechosModel.cs
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Interfaces/IRegistroDesechosModel.cs
@@ -5,9 +5,16 @@ namespace WEB_APP_Panaderia.Interfaces
 	public interface IRegistroDesechosModel
 	{
 		public List<RegistroDesechosEntities> ConsultarRegistroDesechos();
+		public int AgregarRegistroDesecho(RegistroDesechosEntities reporte);
+		public void ActualizarRegistroDesecho(RegistroDesechosEntities reporte);
 
+		public void EliminarRegistroDesecho(int idProveedor);
+		public RegistroDesechosEntities? ConsultarUnRegistroDesecho(int id);
 		public byte[] GenerarPdfRegistroDesechos(List<RegistroDesechosEntities> registros);
-
 		public byte[] GenerarExcelRegistroDesechos(List<RegistroDesechosEntities> registros);
+		public List<CategoriaDesechoEntities> ConsultarCategoriaDesecho();
+		public List<CategoriaDesechoTratamientoEntities> ConsultarCategoriaDesechoTratamiento();
+		public List<DesechoDisposicionFinalEntities> ConsultarDesechoDisposicionFinal();
+		public List<DesechoTransporteEntities> ConsultarDesechoTransporte();
 	}
 }
