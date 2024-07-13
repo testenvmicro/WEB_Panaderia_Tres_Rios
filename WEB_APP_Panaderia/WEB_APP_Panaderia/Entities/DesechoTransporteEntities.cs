845fff3ad090f6b4fd0c0036bf9105b88baae789
testenvmicro <114837714+testenvmicro@users.noreply.github.com>
2024-07-12 22:58:52 -0600

    Add files via upload

---
+++ WEB_APP_Panaderia/WEB_APP_Panaderia/Entities/DesechoTransporteEntities.cs
@@ -0,0 +1,8 @@
+﻿namespace WEB_APP_Panaderia.Entities
+{
+	public class DesechoTransporteEntities
+	{
+		public long idDesechoTransporte { get; set; }
+		public string descripcion { get; set; } = string.Empty;
+	}
+}
